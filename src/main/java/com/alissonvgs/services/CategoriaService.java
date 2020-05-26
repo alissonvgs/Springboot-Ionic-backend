66dd018a9eba70257315de671841e756b0177df4
alissonvgs <alyssonvictor00@gmail.com>
2020-05-26 19:49:09 -0300

    Put e Delete implementado em Categorias + Exceptions no delete usando Bad_Request

--- src/main/java/com/alissonvgs/services/CategoriaService.java
+++ src/main/java/com/alissonvgs/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.alissonvgs.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.alissonvgs.domain.Categoria;
 import com.alissonvgs.repositories.CategoriaRepository;
+import com.alissonvgs.services.exceptions.DataIntegrityException;
 import com.alissonvgs.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,14 +17,29 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 }
