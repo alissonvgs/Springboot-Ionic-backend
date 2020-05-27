9f7d9409922fc915b1daebf7a6ebbbb5bc803feb
alissonvgs <alyssonvictor00@gmail.com>
2020-05-27 18:35:31 -0300

    Metodo POST implementado - Cliente | Mas com error

--- src/main/java/com/alissonvgs/resources/ClienteResource.java
+++ src/main/java/com/alissonvgs/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.alissonvgs.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,15 +12,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alissonvgs.domain.Cliente;
 import com.alissonvgs.dto.ClienteDTO;
+import com.alissonvgs.dto.ClienteNewDTO;
 import com.alissonvgs.services.ClienteService;
 
 @RestController
@@ -43,6 +47,16 @@ public class ClienteResource {
 			obj.setId(id);
 			obj = service.update(obj);
 			return ResponseEntity.noContent().build();
+		}
+		
+		//@RequestMapping( method = RequestMethod.POST)
+		@PostMapping
+		public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+			Cliente obj = service.fromDTO(objDto);
+			obj = service.insert(obj);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();
 		}
 		
 		//@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
