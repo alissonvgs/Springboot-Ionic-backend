a6741a6ebbd2217fc028b7e4c35f4ab5246eae32
alissonvgs <alyssonvictor00@gmail.com>
2020-05-30 14:49:27 -0300

    Validacao costumizada: email ñao repetido no Post do cliente - Cliente

--- src/main/java/com/alissonvgs/repositories/ClienteRepository.java
+++ src/main/java/com/alissonvgs/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.alissonvgs.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alissonvgs.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
