65590b8efa918e3175b09119cae7956bce5f9040
alissonvgs <alyssonvictor00@gmail.com>
2020-06-01 19:06:24 -0300

    Busca de produtos por nome e categoria - Produto

--- src/main/java/com/alissonvgs/dto/ClienteDTO.java
+++ src/main/java/com/alissonvgs/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.alissonvgs.domain.Cliente;
+import com.alissonvgs.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID= 1L;
 	
