b90c7cad5f7a95f67e49569b4dfcdff382378c6a
alissonvgs <alyssonvictor00@gmail.com>
2020-06-16 13:51:09 -0300

    Adição do ToString de pedido

--- src/main/java/com/alissonvgs/domain/ItemPedido.java
+++ src/main/java/com/alissonvgs/domain/ItemPedido.java
@@ -1,6 +1,9 @@
 package com.alissonvgs.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -31,7 +34,7 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 	
-	public double getSubtotal() {
+	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
 
@@ -108,5 +111,21 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome());
+		builder.append(", Qte: ");
+		builder.append(getQuantidade());
+		builder.append(", Preço unitário: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(getSubTotal()));
+		builder.append("\n");
+		return builder.toString();
+	}
+
 
 }
--- src/main/java/com/alissonvgs/services/PedidoService.java
+++ src/main/java/com/alissonvgs/services/PedidoService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alissonvgs.domain.ItemPedido;
 import com.alissonvgs.domain.PagamentoComBoleto;
@@ -13,7 +14,6 @@ import com.alissonvgs.domain.enums.EstadoPagamento;
 import com.alissonvgs.repositories.ItemPedidoRepository;
 import com.alissonvgs.repositories.PagamentoRepository;
 import com.alissonvgs.repositories.PedidoRepository;
-import com.alissonvgs.repositories.ProdutoRepository;
 import com.alissonvgs.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,8 +28,6 @@ public class PedidoService {
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
 	
-	@Autowired
-	private ProdutoRepository produtoRepository;
 	
 	@Autowired
 	private ProdutoService produtoService;
@@ -37,15 +35,20 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 
+	@Autowired
+	private ClienteService clienteService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 	
+	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -56,10 +59,14 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		
+		System.out.println(obj);
+		
 		return obj;
 	}
 
